e6751b2e2deff703f90e47ca0bb0e77e1c047391
60manojkumar@gmail.com <Motivated@2019>
2021-08-24 20:27:55 +0530

    Database connection

--- src/main/java/com/demo/model/Employee.java
+++ src/main/java/com/demo/model/Employee.java
@@ -1,12 +1,25 @@
 package com.demo.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "EMPLOYEE_TABLE_JAVA")
 public class Employee {
+
+    @Id // primary key unique value
+    @GeneratedValue // Automatically increment current 0,1,2,3,......14,15
+            Long id;
 
     String name;
     int age;
     String dept;
 
 
+    // default constructor
+    public Employee() {
+
+    }
     // parameterized constructor
 
 
